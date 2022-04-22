6497f1b6e5612fad9f9a9eb18f674a552d05a33c
ItalianoBaffi <mark_sicoli@yahoo.com>
2022-04-22 07:35:52 -0500

    Update khkGears.py

--- khkGears.py
+++ khkGears.py
@@ -16,86 +16,37 @@ parts = []
 info = [['Name','Bending Strength (N-m)','Surface Strength (N-m)','Pitch Diameter (mm)','Teeth','Weight (kg)','Bore (mm)','Price (1-9)','Price (10-24)','Price (25-49)','Price (50-99)','Price (100-249)']]
 options = Options()
 options.headless = True
-'''
-# Grabs links for each path from main spur/helical gear page
-for url in urls:
-	driver.get(url)
-	modules = driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')
-	# Selects every module
-	for i in range(len(modules)):
-		WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-sort_module')))
-		hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')[i]).perform()
 
-		WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-		teeth = driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')
-		for j in range(len(teeth)):
-			WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-			hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[j]).perform()
-
-			WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID, 'cds-attribute-value-list-no_teeth')))
-			# Ensures every part is displayed on the list
-			try:
-				WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME,'loadmore-link')))
-				while len(driver.find_elements(By.CLASS_NAME,'loadmore-link')) != 0:
-					hover.click(driver.find_elements(By.CLASS_NAME,'loadmore-link btn btn-default')[0]).perform()
-					try:
-						WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME,'loadmore-link')))
-					except:
-						pass
-			except:
-				pass
-		
-			# Grabs and adds link to parts list
-			for partName in driver.find_elements(By.TAG_NAME,'h2'):
-				parts.append(partName.find_elements(By.TAG_NAME,'a')[0].get_attribute('href'))
-
-			WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-			hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[j]).perform()
-			print("Module: {}: {}%, Teeth: {}: {}%".format(i,100*i/len(modules),j,100*j/len(teeth))) # Shows Progress
-
-		WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-sort_module')))
-		hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')[i]).perform()
-		'''
-def grabParts(url, driver):
+def grabParts(url, i, driver, teeth):
 	global parts
 	
 	driver.get(url)
 	hover = ActionChains(driver)
-	modules = driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')
-	# Selects every module
-	for i in range(len(modules)):
-		WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-sort_module')))
-		hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')[i]).perform()
+	
+	# Selects every tooth
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
+	hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[i]).perform()
 
-		WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-		teeth = driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')
-		for j in range(len(teeth)):
-			WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-			hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[j]).perform()
-
-			WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID, 'cds-attribute-value-list-no_teeth')))
-			# Ensures every part is displayed on the list
+	WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID, 'cds-attribute-value-list-no_teeth')))
+	# Ensures every part is displayed on the list
+	try:
+		WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME,'loadmore-link')))
+		while len(driver.find_elements(By.CLASS_NAME,'loadmore-link')) != 0:
+			hover.click(driver.find_elements(By.CLASS_NAME,'loadmore-link btn btn-default')[0]).perform()
 			try:
 				WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME,'loadmore-link')))
-				while len(driver.find_elements(By.CLASS_NAME,'loadmore-link')) != 0:
-					hover.click(driver.find_elements(By.CLASS_NAME,'loadmore-link btn btn-default')[0]).perform()
-					try:
-						WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME,'loadmore-link')))
-					except:
-						pass
 			except:
 				pass
-		
-			# Grabs and adds link to parts list
-			for partName in driver.find_elements(By.TAG_NAME,'h2'):
-				parts.append(partName.find_elements(By.TAG_NAME,'a')[0].get_attribute('href'))
+	except:
+		pass
 
-			WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
-			hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[j]).perform()
-			print("Module: {}: {}%, Teeth: {}: {}%".format(i,100*i/len(modules),j,100*j/len(teeth))) # Shows Progress
+	# Grabs and adds link to parts list
+	for partName in driver.find_elements(By.TAG_NAME,'h2'):
+		parts.append(partName.find_elements(By.TAG_NAME,'a')[0].get_attribute('href'))
 
-		WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-sort_module')))
-		hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-sort_module').find_elements(By.TAG_NAME,'input')[i]).perform()
+	WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'cds-attribute-value-list-no_teeth')))
+	hover.click(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input')[i]).perform()
+	print("Tooth Progress: {:.2f}%".format(100*i/teeth)) # Shows Progress
 
 # Gathers information from each part page and adds it to info list
 def grabContent(part, driver):
@@ -103,7 +54,6 @@ def grabContent(part, driver):
 	global parts
 
 	driver.get(part)
-	temp = []
 
 	# Searches for relevant data in list and assigns value to variable
 	for label in driver.find_elements(By.CLASS_NAME,'label'):
@@ -122,29 +72,37 @@ def grabContent(part, driver):
 			bore = label.find_element(By.XPATH,'//following-sibling::div').get_attribute('textContent')
 
 	# Creates a list of all the information pulled
-	temp.append('<a href="'+part+'">'+driver.find_elements(By.CLASS_NAME,'productName')[0].get_attribute('textContent').spit('\n\t\t\t\t\t\t\t')[1]+'</a>',bending,surface,pitch,teeth,weight,bore,
+	temp = ['<a href="'+part+'">'+driver.find_elements(By.CLASS_NAME,'productName')[0].get_attribute('textContent').spit('\n\t\t\t\t\t\t\t')[1]+'</a>',bending,surface,pitch,teeth,weight,bore,
 			 driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[1],driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[2],
-			 driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[3],driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[4],driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[5])
+			 driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[3],driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[4],driver.find_elements(By.TAG_NAME,'tr')[1].find_elements(By.TAG_NAME,'td')[5]]
 
 	# Adds list of information to main info list
 	info.append(temp)
 
-	print("Progress {}%".format(100*len(info)/len(parts))) # Shows Progress
+	print("Content Progress {:.2f}%".format(100*len(info)/len(parts))) # Shows Progress
 
 
 if __name__ == "__main__":
-	# Handles multithreading of pulling information
-	drivers = [webdriver.Chrome(options=options) for _ in range(len(urls))]
-	prt = cf.ThreadPoolExecutor()
-	prt.map(grabParts,urls,drivers)
+	print("Starting...")
+	# Handles multithreading based on tooth count
+	for url in urls:
+		driver.webdriver.Chrome(options=options)
+		driver.get(url)
+		teeth = len(driver.find_element(By.ID, 'cds-attribute-value-list-no_teeth').find_elements(By.TAG_NAME,'input'))
+		driver.quit()
+		drivers = [webdriver.Chrome(options=options) for _ in range(len(urls))]
+		prt = cf.ThreadPoolExecutor()
+		prt.map(grabParts,[url for _ in range(teeth)],list(range(teeth)),drivers,teeth)
+		[driver.quit() for driver in drivers]
+	print("Url fetching complete!")
 
-	[driver.quit() for driver in drivers]
-
+	# Handles multithreading for data fetching
 	drivers = [webdriver.Chrome(options=options) for _ in range(len(parts))]
 	ex = cf.ThreadPoolExecutor()
 	ex.map(grabContent,parts,drivers)
 
 	[driver.quit() for driver in drivers]
+	print("Data fetching complete!")
 
 	# Creates and writes information to file
 	with open('./outputFiles/kgkGearsOutput '+datetime.datetime.now().strftime("%Y-%m-%d %H_%M_%S")+'.csv','w+') as file:
